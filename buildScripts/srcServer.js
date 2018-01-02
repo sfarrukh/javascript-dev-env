e0a3b7a768b2fae40cc78f3ae8971c4cf86d8163
Farrukh <farrukh@Farrukhs-MacBook-Pro.local>
2018-01-01 17:35:38 -0800

    Choose fetch for making HTTP call on browser

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -27,6 +27,13 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+    res.json([
+        {"id": 1, "firstName":"Ron", "lastName":"Smith", "email": "ron@website.com"},
+        {"id": 1, "firstName":"Kevin", "lastName":"Ice", "email": "kevin@website.com"}
+    ]);
+});
+
 /* eslint-disable no-console */
 
 // Tell express to listen to port defined above
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import './index.css';
-import numeral from 'numeral';
+// import './index.css';
+// import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00');
+// const courseValue = numeral(1000).format('$0,0.00');
 // debugger;
-console.log(`I would pay ${courseValue} for this course`); // eslint-disable-line no-console
+// console.log(`I would pay ${courseValue} for this course`); // eslint-disable-line no-console
+
+import { getUsers } from './api/userApi';
+
+// Populate table of users via API call
+getUsers().then(result => {
+    let usersBody = "";
+    
+    result.forEach(user => {
+        usersBody += `<tr>
+                <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+                <td>${user.id}</td>
+                <td>${user.firstName}</td>
+                <td>${user.lastName}</td>
+                <td>${user.email}</td>
+            </tr>`
+    });
+
+    global.document.getElementById('users').innerHTML = usersBody;
+});
