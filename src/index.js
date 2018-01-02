bb6587cbd0703a2276ebab99f6dfedba7165e85d
Farrukh <farrukh@Farrukhs-MacBook-Pro.local>
2018-01-01 21:38:35 -0800

    Generate JSON-data schema with JSON Schema Fake to generate JSON data, & Serve mock-JSON-api with json-server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 // debugger;
 // console.log(`I would pay ${courseValue} for this course`); // eslint-disable-line no-console
 
-import { getUsers } from './api/userApi';
+import { getUsers, deleteUser } from './api/userApi';
 
 // Populate table of users via API call
 getUsers().then(result => {
@@ -22,4 +22,18 @@ getUsers().then(result => {
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    // Must use array.from to create a real array from a DOM collection
+    // getElementsByClassname only returns an "array like" object
+    Array.from(deleteLinks, link => {
+      link.onclick = function(event) {
+        const element = event.target;
+        event.preventDefault();
+        deleteUser(element.attributes["data-id"].value);
+        const row = element.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+      };
+    });
 });
