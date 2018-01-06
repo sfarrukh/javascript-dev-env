1ed27daed6a563132b429be690269cabef67357f
Farrukh <farrukh@Farrukhs-MacBook-Pro.local>
2018-01-05 19:55:45 -0800

    +Prod Build: Add scripts in /package.json to run production-build environment

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -16,7 +16,7 @@ export default {
   },
   plugins: [
     // Eliminate duplicate packages when generating bundle
-    new webpack.optimize.DeduePlugin(),
+    new webpack.optimize.DedupePlugin(),
     // Minify JS
     new webpack.optimize.UglifyJsPlugin()
   ],
