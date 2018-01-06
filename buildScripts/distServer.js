01d37fef1d17f78333ec978a3647933c44f4fd06
Farrukh <farrukh@Farrukhs-MacBook-Pro.local>
2018-01-05 19:11:09 -0800

    +Prod Build: Create /buildScripts/distServer.js to serve files in /dist directory

---
+++ buildScripts/distServer.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.get('/users', function (req, res) {
+  res.json([
+      {"id": 1, "firstName":"Ron", "lastName":"Smith", "email": "ron@website.com"},
+      {"id": 1, "firstName":"Kevin", "lastName":"Ice", "email": "kevin@website.com"}
+  ]);
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
